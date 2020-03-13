394586ff90b280de6fe919ae08129457cff4d466
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2020-03-13 14:47:22 +0530

    added swift package, dependency, ios version

--- FantasticFeature/Package.swift
+++ FantasticFeature/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FantasticFeature",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -14,13 +15,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FantasticFeature",
-            dependencies: []),
+            dependencies: [.product(name: "Alamofire")]),
         .testTarget(
             name: "FantasticFeatureTests",
             dependencies: ["FantasticFeature"]),
---
+++ FantasticFeature/Sources/FantasticFeature/Foo.swift
@@ -0,0 +1,14 @@
+
+//
+//  File.swift
+//  
+//
+//  Created by sunil.kumar1 on 3/13/20.
+//
+
+import Foundation
+import Alamofire
+
+class Foo {
+    
+}
